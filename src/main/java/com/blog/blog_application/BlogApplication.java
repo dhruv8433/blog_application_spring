ac008ba5b8fef83a8160858b28183fedca9efc3b
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-03-13 19:17:29 +0530

    update main app and test for password encoder

--- src/main/java/com/blog/blog_application/BlogApplication.java
+++ src/main/java/com/blog/blog_application/BlogApplication.java
@@ -1,15 +1,21 @@
 package com.blog.blog_application;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.blog.blog_application.model.User;
 import com.blog.blog_application.payload.UserDto;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication implements CommandLineRunner{
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
@@ -24,6 +30,11 @@ public class BlogApplication {
 				.addMappings(mapper -> mapper.skip(User::setId));
 
 		return modelMapper;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(this.passwordEncoder.encode("abc"));
 	}
 
 }
